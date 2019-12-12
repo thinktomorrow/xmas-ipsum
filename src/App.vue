fae5db07dc7bdcbecdc8c0fd498d11f31dccb62c
TijsTT <tijs@thinktomorrow.be>
2019-12-12 16:56:40 +0100

    animation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <div class="text-center m-16">
+  <div id="app" class="w-3/5 mx-auto py-16">
+    <!-- <div class="text-center m-16">
       <svg class="inline-block h-12 w-12">
         <path fill="currentColor" d="M25.4,6C13.7,6,4.2,14.5,4.2,25c0,3.4,1,6.6,2.8,9.4l-3.5,8.9l8.4-3.6c3.6,2.7,8.3,4.3,13.4,4.3c11.7,0,21.1-8.5,21.1-19S37.1,6,25.4,6L25.4,6z"></path>
         <path fill="#FFFFFF" d="M36.9,21.5v-2h-4v-4h-5v4h-7v-4h-5v4h-3v2h3v8.9c0,1.4,0.4,2.4,1.1,3.1c0.7,0.7,1.8,1.1,3.3,1.1c1,0,1.9-0.1,2.6-0.3c0.7-0.2,1-0.5,2-0.7v-1.2c0,0-0.5,0.1-1,0.1s-0.8,0.1-1.1,0.1c-0.8,0-1.3-0.2-1.6-0.7s-0.3-1.3-0.3-2.4v-8h7v8.9c0,1.4,0.2,2.4,1,3.1c0.7,0.7,1.8,1.1,3.3,1.1c1,0,2-0.1,2.7-0.3s1.1-0.5,2.1-0.7v-1.2c0,0-0.6,0.1-1.1,0.1s-0.9,0.1-1.2,0.1c-0.8,0-1.2-0.2-1.5-0.7s-0.3-1.3-0.3-2.4v-8H36.9z"></path>
       </svg>
-    </div>
+    </div> -->
     <page-generate v-if="page == 'generate'"></page-generate>
     <page-result v-if="page == 'result'"></page-result>
   </div>
@@ -23,20 +23,10 @@ export default {
   },
   data(){
     return {
-      page: 'result',
+      page: 'generate',
     }
   }
 }
 </script>
 
 <style src="./assets/main.css" />
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Pages/Result.vue
+++ src/components/Pages/Result.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        <h1>Some great lines of joy</h1>
+        <h1 class="text-center">Some great lines of joy</h1>
 
-        <p class="p-8">
-            generated text
-        </p>
+        <div class="w-3/5 mx-auto my-8 rounded bg-gray-100 p-8 border border-gray-200">
+            <p id="generated-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
 
-        <button class="p-4 bg-red-400 text-white">copy to clipboard</button>
-        <button class="p-4 bg-red-300 text-white">try again</button>
+        <div class="flex justify-center">
+            <button class="btn bg-red-500 mr-2">copy to clipboard</button>
+            <button class="btn bg-red-400">try again</button>
+        </div>
 
     </div>
 </template>
