18b22e25e21a3bda779d5237d22908278eaa6e4e
Philippe Damen <philippe@thinktomorrow.be>
2019-12-12 14:35:15 +0100

    add sentence formats

--- src/components/Sentences/SentenceGenerator.js
+++ src/components/Sentences/SentenceGenerator.js
@@ -1,7 +1,16 @@
+export let sentenceFormats = [
+    ['adjectives', 'nouns', 'adverbs', 'verbs', 'nouns', 'adverbs', 'verbs'],
+    ['adjectives', 'nouns', 'verbs'],
+    ['adjectives', 'nouns', 'adverbs', 'verbs', 'nouns'],
+    ['nouns', 'verbs', 'adverbs', 'nouns'],
+    ['nouns', 'verbs', 'adjectives', 'nouns'],
+    ['nouns', 'verbs', 'nouns'],
+    ['adjectives', 'nouns', 'adverbs', 'verbs', 'preposition', 'adjectives', 'nouns'],
+]
+
 export default{
 
     generate(numberOfSentences = 20, mood = "neutral"){
-        console.log('dididi');
 
         // nouns
         // verbs
